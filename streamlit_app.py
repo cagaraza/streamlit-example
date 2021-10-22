fb719628f1c792e0788a263a1ed2bed5f5c95c56
christian.garaza <christian.garaza@blaize.com>
2021-10-22 23:04:45 +0800

    sample app

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,24 +15,60 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+import requests
+
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    area_home = st.text_input("area_home", area_home)
+    age_home = st.text_input("age_home", age_home)
+    number_people = st.text_input("number_people", number_people)
+    age_windows = st.text_input("age_windows", age_windows)
+    number_panes = st.text_input("number_panes", numbe_panes])
+    old_type_heating = st.text_input("old_type_heating", old_type_heating)
+    number_floor = st.text_input("number_floor", number_floor)
+        # 'area_home' : 140, #average area of livable space in germany
+        # 'age_home' : 1960,
+        # 'number_people' : 3,
+        # 'age_windows' : 1980,
+        # 'number_panes' : 1,
+        # 'age_heating' : 1990,
+        # 'old_type_heating' : 0, #oil heating
+        # 'number_floor' : 2
 
-    points_per_turn = total_points / num_turns
+    # url = 'https://interhyp-dps.ew.r.appspot.com/'
+    url = 'https://mrpeach-dot-interhyp-dps.ew.r.appspot.com'
+    # url = 'http://127.0.0.1:5000'
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    params = {'area_home' : 140, #average area of livable space in germany
+            'age_home' : 1960,
+            'number_people' : 3,
+            'age_windows' : 1980,
+            'number_panes' : 1,
+            'age_heating' : 1990,
+            'old_type_heating' : 0, #oil heating
+            'number_floor' : 2
+            }
+    response = requests.post(url, params)
+    print(params)
+    print(response.json())
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+    # Point = namedtuple('Point', 'x y')
+    # data = []
+
+    # points_per_turn = total_points / num_turns
+
+    # for curr_point_num in range(total_points):
+    #     curr_turn, i = divmod(curr_point_num, points_per_turn)
+    #     angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+    #     radius = curr_point_num / total_points
+    #     x = radius * math.cos(angle)
+    #     y = radius * math.sin(angle)
+    #     data.append(Point(x, y))
+
+    # st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+    #     .mark_circle(color='#0068c9', opacity=0.5)
+    #     .encode(x='x:Q', y='y:Q'))
