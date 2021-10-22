6bfc46cd21ad0526f2e59621ca30eaa308621e28
christian.garaza <christian.garaza@blaize.com>
2021-10-22 23:07:48 +0800

    sample app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,28 +5,9 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome to Mr. Peach!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-import requests
-
-
-with st.echo(code_location='below'):
-
-    area_home = st.text_input("area_home", area_home)
-    age_home = st.text_input("age_home", age_home)
-    number_people = st.text_input("number_people", number_people)
-    age_windows = st.text_input("age_windows", age_windows)
-    number_panes = st.text_input("number_panes", number_panes)
-    old_type_heating = st.text_input("old_type_heating", old_type_heating)
-    number_floor = st.text_input("number_floor", number_floor)
+# Sample Input
         # 'area_home' : 140, #average area of livable space in germany
         # 'age_home' : 1960,
         # 'number_people' : 3,
@@ -35,6 +16,31 @@ with st.echo(code_location='below'):
         # 'age_heating' : 1990,
         # 'old_type_heating' : 0, #oil heating
         # 'number_floor' : 2
+
+# Sample output
+        # {'window': {'funding': 20, 'renovate': 1, 'cost': 12000, 'savings': 440}, 'heating': {'funding': '50', 'renovate': 1, 'cost': '15000', 'savings': '0'}}
+"""
+
+import requests
+
+
+with st.echo(code_location='below'):
+
+    area_home = st.text_input("area_home")
+    age_home = st.text_input("age_home")
+    number_people = st.text_input("number_people")
+    age_windows = st.text_input("age_windows")
+    number_panes = st.text_input("number_panes")
+    old_type_heating = st.text_input("old_type_heating")
+    number_floor = st.text_input("number_floor")
+    # 'area_home' : 140, #average area of livable space in germany
+    # 'age_home' : 1960,
+    # 'number_people' : 3,
+    # 'age_windows' : 1980,
+    # 'number_panes' : 1,
+    # 'age_heating' : 1990,
+    # 'old_type_heating' : 0, #oil heating
+    # 'number_floor' : 2
 
     # url = 'https://interhyp-dps.ew.r.appspot.com/'
     url = 'https://mrpeach-dot-interhyp-dps.ew.r.appspot.com'
